91748cfc8635851eb240fa893508395115f7fc5a
Raees <imfura2001@hotmail.com>
2026-05-07 12:18:32 -0400

    LOG-172: added subscribers

--- include/sonia_blackbox/BlackBox.hpp
+++ include/sonia_blackbox/BlackBox.hpp
@@ -1,15 +1,31 @@
 #pragma once
 
 #include <rclcpp/rclcpp.hpp>
+#include <sonia_common_ros2/msg/battery_power_messages.hpp>
+#include <sonia_common_ros2/msg/motor_power_messages.hpp>
+#include <sonia_common_ros2/msg/body_velocity_dvl.hpp>
+#include <std_msgs/msg/float32.hpp>
+#include <sensor_msgs/msg/imu.hpp>
 
 namespace sonia_blackbox{
+
     class BlackBox : public rclcpp::Node
     {
         public:
             BlackBox();
-            ~BlackBox() = default;
+            ~BlackBox() override = default;
+
         private:
+            void BatteryCallback(const sonia_common_ros2::msg::BatteryPowerMessages &msg);
+            void MotorCallback(const sonia_common_ros2::msg::MotorPowerMessages &msg);
+            void DvlCallback(const sonia_common_ros2::msg::BodyVelocityDVL &msg);
+            void DepthCallback(const std_msgs::msg::Float32 &msg);
+            void ImuCallback(const sensor_msgs::msg::Imu &msg);
 
-
+            rclcpp::Subscription<sonia_common_ros2::msg::BatteryPowerMessages>::SharedPtr battery_sub;
+            rclcpp::Subscription<sonia_common_ros2::msg::MotorPowerMessages>::SharedPtr motor_sub;
+            rclcpp::Subscription<sonia_common_ros2::msg::BodyVelocityDVL>::SharedPtr dvl_sub;
+            rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imu_sub;
+            rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr depth_sub;
     };
 }
--- src/BlackBox.cpp
+++ src/BlackBox.cpp
@@ -1,9 +1,35 @@
 #include "sonia_blackbox/BlackBox.hpp"
 
+using std::placeholders::_1;
 namespace sonia_blackbox{
     BlackBox::BlackBox(): Node("provider_blackbox")
     {
+        battery_sub = create_subscription<sonia_common_ros2::msg::BatteryPowerMessages>("/provider_power/battery_voltages",10, std::bind(&BlackBox::BatteryCallback, this, _1));
+        motor_sub =create_subscription<sonia_common_ros2::msg::MotorPowerMessages>("/provider_power/motor_voltages",10, std::bind(&BlackBox::MotorCallback, this, _1));
+        dvl_sub = create_subscription<sonia_common_ros2::msg::BodyVelocityDVL>("/provider_dvl/dvl_velocity",10, std::bind(&BlackBox::DvlCallback, this, _1));
+        depth_sub = create_subscription<std_msgs::msg::Float32>("provider_depth/depth",10, std::bind(&BlackBox::DepthCallback, this, _1));
+        imu_sub = create_subscription<sensor_msgs::msg::Imu>("provider_imu/imu_info",10, std::bind(&BlackBox::ImuCallback, this, _1));
         
+    }
+    void BlackBox::BatteryCallback(const sonia_common_ros2::msg::BatteryPowerMessages &msg)
+    {
+    }
+
+    void BlackBox::MotorCallback(const sonia_common_ros2::msg::MotorPowerMessages &msg)
+    {
+    }
+
+    void BlackBox::DvlCallback(const sonia_common_ros2::msg::BodyVelocityDVL &msg)
+    {
+    }
+
+    void BlackBox::DepthCallback(const std_msgs::msg::Float32 &msg)
+    {
+    }
+
+    void BlackBox::ImuCallback(const sensor_msgs::msg::Imu &msg)
+    {
     }
 
 }
+
